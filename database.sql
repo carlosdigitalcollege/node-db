8fcf52b74433c181c767c80b8e8d6d21bc33a127
carlosdigitalcollege <111659773+carlosdigitalcollege@users.noreply.github.com>
2022-11-26 09:22:20 -0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,9 +1,5 @@
 create database loja;
 use loja;
-show tables;
-desc product;
-
-select * from product;
 
 create table product (
    id INT NOT NULL AUTO_INCREMENT,
@@ -16,3 +12,5 @@ create table product (
 INSERT INTO product (name, price, description) VALUES ('Meia', 14.99, 'ksadklasjj salkdjaskl djaslkdja slkdjas ldj lk');
 INSERT INTO product (name, price, description) VALUES ('Camisa', 124.99, 'ksadklasjj salkdjaskl djaslkdja slkdjas ldj lk');
 INSERT INTO product (name, price, description) VALUES ('Tenis', 140.99, 'O tenis é ksadklasjj salkdjaskl djaslkdja slkdjas ldj lk');
+
+select * from product;
